71ea729e77f57aaddfc1ae7459ebdb02e1be5912
Milo Sanu <camilosanu@gmail.com>
2016-10-27 23:59:14 -0400

    Changed show to show2

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use App\Kerbalnaut;
 use App\Mission;
 //use App\Http\Controllers\Controller;
-//use Illuminate\Http\Request;
-use App\Http\Requests;
+use Illuminate\Http\Request;
+//use App\Http\Requests\Request;
 
 class PagesController extends Controller
 {
@@ -26,5 +26,20 @@ class PagesController extends Controller
 //        Grabbing all the missions for the mission's homepage
         $missions = Mission::all();
         return view('pages.mission',compact('missions'));
+    }
+
+    public function add_kerbalnaut(Request $requests) {
+
+        $kerbal = new Kerbalnaut(['last_name'=>$requests->last_name,
+            'first_name'=>$requests->first_name,
+            'gender'=>$requests->gender,
+            'role'=> $requests->role,
+            'hire_date'=> $requests->hire_date,
+            'mission_status'=> 'Ready for Mission',
+            'portrait'=>'http://wiki.kerbalspaceprogram.com/images/e/ee/Happy_kerbal.jpg',
+            'is_alive'=>1]);
+        $kerbal->save();
+        return redirect('team');
+
     }
 }
